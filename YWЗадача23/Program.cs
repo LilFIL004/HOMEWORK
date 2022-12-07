71a34846eb067da5ee29b5c313a5a7c6fcc76bf1
LILIYA <kirpichka4a@mail.ru>
2022-12-07 21:30:33 +0300

    YW Задача 23

---
+++ YWЗадача23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число(N) и выдаёт таблицу кубов чисел oт 1 до N
+// 3 -> 1,4,9
+// 5 -> 1,8,27,64,125
+
+Console.WriteLine("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int a = 1; a <= num; a  ++)
+{
+    Console.WriteLine($"Исходное число: {a}");
+    Console.WriteLine($"Возведённое число в куб: {Math.Pow(a, 3)}");
+}
