1f85e3b674a2982316b69511bd9ad96bbcfb6b09
LILIYA <kirpichka4a@mail.ru>
2022-12-11 00:34:44 +0300

    HW Задача 29

---
+++ HWЗадача29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, котрая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+pechatMassiv(Numbers(numArray(8)));
+int[] numArray(int arrayLenght)
+{
+    int[] chislo = new int [arrayLenght];
+    return chislo;
+}
+int[] Numbers(int[]num)
+{
+    for (int a = 0; a < num.Length; a ++)
+    {
+        num[a] = new Random().Next(0, 50);
+    }
+     return num;
+}
+void pechatMassiv(int[] array)
+{
+    foreach(int a in array)
+    {
+      Console.WriteLine($"{a}");
+    }
+}
