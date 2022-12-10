ac1b4dbefc83df64cdbdd0a3403725d15d1142bf
LILIYA <kirpichka4a@mail.ru>
2022-12-11 00:33:27 +0300

    YW Задача 27

---
+++ YWЗадача27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите любое не однозначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр {num} = {SumIzNumbers(num)}");
+int SumIzNumbers(int a)
+{
+    int result = 0;
+    while (a > 10)
+    {
+        result += a % 10;
+        a = a / 10;
+    }
+    if (a < 10)
+    {
+        result += a;
+    }
+    return result;
+}
