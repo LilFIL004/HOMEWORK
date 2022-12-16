eb234f1b64580933e87529a0e51c2f3461529b3a
LILIYA <kirpichka4a@mail.ru>
2022-12-16 17:45:31 +0300

    HW Задача 34

---
+++ HWЗадача34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами.Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] array = new int [10]; 
+DoArray(array);
+Console.WriteLine(" : Случайные трёхзначные числа ");
+soThat (array);
+
+void soThat (int [] array)
+{
+     int count=0;
+    foreach(int ar in array)
+    { 
+         if ((ar & 1) ==0)
+    {
+           count++;
+    }
+    }
+    Console.WriteLine($"Число положительных чисел {count}");
+} 
+void DoArray (int [] array)
+{
+     for (int i = 0; i < array.Length; i ++)
+     {
+        array[i] = new Random().Next(100, 1000);
+        Console.Write($"{array[i]} ");
+     }
+}
